eb137545c6cc8f0ea2b73d689387d930a41584f3
Nataliia Pashchenko <nvpashchenko@ukr.net>
2024-04-08 18:08:25 +0200

    add: drag end drop string

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 const btn = document.querySelector(".btn");
 let data = document.querySelector(".inputString");
 let element = document.querySelector(".printString");
+let dragging = false;
+let startX = 0;
+let startY = 0;
 
 btn.addEventListener('click', () => {
     element.textContent = data.value;
 })
+
+element.onmousedown = function(event) {
+
+  document.body.append(element);
+
+
+  function moveAt(pageX, pageY) {
+    element.style.left = pageX - element.offsetWidth / 2 + 'px';
+    element.style.top = pageY - element.offsetHeight / 2 + 'px';
+  }
+
+  
+  moveAt(event.pageX, event.pageY);
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+ 
+  document.addEventListener('mousemove', onMouseMove);
+
+
+  element.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    element.onmouseup = null;
+  };
+
+};
+
